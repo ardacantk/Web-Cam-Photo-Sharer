bd197a5d8ce024178a9b66f6cc77222dc9eba6ef
Arda Can Tekküpeli <ardacan.tkpl@gmail.com>
2022-07-31 01:10:30 +0300

    PC' Web Cam Photo Sharer
    Take photo from your PC's webcam. Save it on your PC or crate a link on File Stack.

---
+++ Webcamshare.py
@@ -0,0 +1,59 @@
+import webbrowser
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from kivy.core.clipboard import Clipboard
+import time
+from fileshare import FileSharer
+
+Builder.load_file("frontend.kv")
+
+class CameraScreen(Screen):
+    def start(self):
+        self.ids.camera.opacity = 1
+        self.ids.camera.play = True
+        self.ids.camera_button.text = "Stop Camera"
+        self.ids.camera.texture = self.ids.camera._camera.texture
+
+    def stop(self):
+        self.ids.camera.play = False
+        self.ids.camera_button.text = "Start Camera"
+        self.ids.camera.texture = None
+
+    def capture(self):
+        current_time = time.strftime("%Y%m%d-%H%M%S")
+        self.filepath = f"files/{current_time}.png"
+        self.ids.camera.export_to_png(self.filepath)
+        self.manager.current = "image_screen"
+        self.manager.current_screen.ids.img.source = self.filepath
+
+
+class ImageScreen(Screen):
+    link_message = "Create a Link First !"
+    def create_link(self):
+        file_path = App.get_running_app().root.ids.camera_screen.filepath
+        fileshare = FileSharer(filepath=file_path)
+        self.url = fileshare.share()
+        self.ids.link.text = self.url
+
+    def copy_link(self):
+        try:
+            Clipboard.copy(self.url)
+        except:
+            self.ids.link.text = self.link_message
+
+    def open_link(self):
+        try:
+            webbrowser.open(self.url)
+        except:
+            self.ids.link.text = self.link_message
+
+
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+MainApp().run()
